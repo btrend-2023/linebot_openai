eb388ed93b710b1ec23c85b3b642c5003688449f
btrend-2023 <137664145+btrend-2023@users.noreply.github.com>
2023-06-26 15:24:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,17 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    #response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
     #response = 'test from openai'
     print('**********************************test after openai')
     print(response)
